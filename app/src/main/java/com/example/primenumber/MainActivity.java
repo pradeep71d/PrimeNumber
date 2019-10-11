808b122c41c0ae431e65b53dfb8787266ff51b5b
sandeep <sandeep@ashmar.in>
2019-10-11 18:05:19 +0530

    add

---
+++ app/src/main/java/com/example/primenumber/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.primenumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int i=0; int m=0; int flag=0;
+        int n=3; // to be checked prime or not.
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" is not prime number");
+        }else{
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    System.out.println(n+" is not prime number");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)  { System.out.println(n+" is prime number"); }
+        }//end of else
+    }
+}
+
+
